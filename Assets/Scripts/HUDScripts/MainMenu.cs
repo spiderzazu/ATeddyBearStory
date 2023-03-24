7c1ed28cd586df8b1f2564ddcfd941711f6e7cfd
spiderzazu <zazuthehedgehog33@yahoo.com.mx>
2023-03-24 14:45:02 -0600

    Menú principal ajustes

--- Assets/Scripts/HUDScripts/MainMenu.cs
+++ Assets/Scripts/HUDScripts/MainMenu.cs
@@ -2,19 +2,31 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class MainMenu : MonoBehaviour
 {
-    public GameObject mainCanvas, aboutCanvas, slotCanvas;
+    public GameObject mainCanvas, aboutCanvas, slotCanvas, tutorialCanvas;
     public SelectedSave saveManager;
     public PlayerInfo defaultSave;
+    public TextMeshProUGUI[] vidas;
+    public TextMeshProUGUI[] energia;
+    public TextMeshProUGUI[] fragmentos;
+    public TextMeshProUGUI[] savePoints;
 
     private void Start()
     {
         SetSavedData();
+        CleanHUD();
         mainCanvas.SetActive(true);
+    }
+
+    public void CleanHUD()
+    {
+        mainCanvas.SetActive(false);
         aboutCanvas.SetActive(false);
         slotCanvas.SetActive(false);
+        tutorialCanvas.SetActive(false);
     }
 
     public void Jugar()
@@ -24,20 +36,25 @@ public class MainMenu : MonoBehaviour
 
     public void Slots()
     {
-        mainCanvas.SetActive(false);
+        CleanHUD();
         slotCanvas.SetActive(true);
+    }
+
+    public void Tutorial()
+    {
+        CleanHUD();
+        tutorialCanvas.SetActive(true);
     }
 
     public void About()
     {
+        CleanHUD();
         aboutCanvas.SetActive(true);
-        mainCanvas.SetActive(false);
     }
 
     public void Back()
     {
-        aboutCanvas.SetActive(false);
-        slotCanvas.SetActive(false);
+        CleanHUD();
         mainCanvas.SetActive(true);
     }
 
@@ -62,7 +79,25 @@ public class MainMenu : MonoBehaviour
 
     private void SetSavedData()
     {
-
+        PlayerInfo[] datos = new PlayerInfo[3] { saveManager.saveFiles[0], saveManager.saveFiles[1], saveManager.saveFiles[2] };
+        for(int i = 0; i < 3; i++)
+        {
+            vidas[i].text = "Vidas " + datos[i].totalLifePoints.ToString();
+            energia[i].text = "Energia " + datos[i].totalAbilityPoints.ToString();
+            fragmentos[i].text = "Fragmentos recogidos " + datos[i].leafFragments.ToString();
+            switch (datos[i].savePoint)
+            {
+                case 0:
+                    savePoints[i].text = "Cueva de los inicios";
+                    break;
+                case 1:
+                    savePoints[i].text = "Fuente de guardado";
+                    break;
+                default:
+                    savePoints[i].text = "Cueva de los inicios";
+                    break;
+            }
+        }
     }
 
     private void NewGameFile(int file)
